30850a136bb1c1c55cdafc9f93c01b7cabe14c5f
Dan Miller <dmiller@windmill.engineering>
2019-12-12 11:33:18 -0500

    add tag, adjust timestamp

--- example.rb
+++ example.rb
@@ -8,14 +8,21 @@ require 'time'
 api_key = ENV["DD_API_KEY"]
 $dog = Dogapi::Client.new(api_key)
 
+$i = 0
+
 def send_metrics(points)
-  # TODO(dmiller): does this tags parameter work?
-  $dog.emit_points('tilt.updates', points, :tags => {})
+  $dog.emit_points('tilt.updates', points, :tags => {user: 'dmiller'}, :type => 'gauge')
+end
+
+def adjust_timestamp(time)
+  $i = $i + 1
+  new_t = Time.now - $i * 60
+  puts new_t
+  new_t
 end
 
 raw_json = ARGF.read
 parsed_json = JSONL.parse(raw_json)
 
-#TODO(dmiller): do I need to massage these timestamps to that they are at most an hour ago?
-points = parsed_json.map { |j| [Time.parse(j["StartTime"]), 1] }
+points = parsed_json.map { |j| [adjust_timestamp(Time.parse(j["StartTime"])), 1] }
 send_metrics(points)
